61685736d73f7dac49d8bc0ee6908ffb2ce95879
Emelia Sena Cobblah <emelia.cobblah@kingmakers.com>
2022-05-21 10:17:51 +0000

    Updated comment Increased readability for interactive project

--- bikeshare1.py
+++ bikeshare1.py
@@ -223,6 +223,7 @@ def main():
         user_stats(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
+        #restarts the data cycle
         if restart.lower() != 'yes':
             break
 
